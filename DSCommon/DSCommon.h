6a1b9e5c79f71c1d759b1bdfdfd4f9a0b1079464
SincereD <sincere129120@icloud.com>
2016-03-13 20:03:54 +0800

    新增类别

---
+++ DSCommon/DSCommon.h
@@ -0,0 +1,17 @@
+//
+//  DSCommon.h
+//  DSCommon
+//
+//  Created by Sincere on 16/3/11.
+//  Copyright © 2016年 Sincere. All rights reserved.
+//
+
+#ifndef DSCommon_h
+#define DSCommon_h
+
+#import "UIColor+DSHexColor.h"
+#import "UIView+DSCapture"
+#import "UIScreen+DSScreenSize"
+#import "NSString+DSEncrypt"
+
+#endif /* DSCommon_h */
