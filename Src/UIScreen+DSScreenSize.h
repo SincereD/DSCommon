ae04780e9f00f436a1e67cade6ed14522379a0b4
SincereD <sincere129120@icloud.com>
2016-03-27 15:02:06 +0800

    新建Src文件夹

---
+++ Src/UIScreen+DSScreenSize.h
@@ -0,0 +1,17 @@
+//
+//  UIScreen+DSScreenSize.h
+//  DSCommon
+//
+//  Created by Sincere on 16/3/11.
+//  Copyright © 2016年 Sincere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScreen (DSScreenSize)
+
++ (CGFloat)screenWidth;
+
++ (CGFloat)screenHeight;
+
+@end
